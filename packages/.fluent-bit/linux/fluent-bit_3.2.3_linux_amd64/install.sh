e4cb94139f5e5927b21de549ad2f02b8fd68f2fb
David Haifley <dhaifley@gmail.com>
2024-12-30 15:47:11 -0500

    feat(OPL-11166): Update fluent-bit linux package with md5 check.

--- packages/.fluent-bit/linux/fluent-bit_3.2.3_linux_amd64/install.sh
+++ packages/.fluent-bit/linux/fluent-bit_3.2.3_linux_amd64/install.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
-wget https://raw.githubusercontent.com/fluent/fluent-bit/master/install.sh | sh
+DIR=$PWD
 
-systemctl disable fluent-bit
+wget -O fbinstall.sh https://raw.githubusercontent.com/fluent/fluent-bit/master/install.sh
+
+if $DIR/checkmd5 --hash=d9a311aba2b23f9e3d87a3eec426d1ae8984f88f5126e256e25bae4f9fb10fc07db563e3d56e9ef5098a8976fca27939377bd48871d28040bbf2d9841fbdfe40 --file=fbinstall.sh; then
+    chmod +x ./fbinstall.sh;
+    ./fbinstall.sh;
+    rm fbinstall.sh;
+    systemctl disable fluent-bit;
+else
+    echo "Downloaded install files do not match checksum";
+    exit 1;
+fi
