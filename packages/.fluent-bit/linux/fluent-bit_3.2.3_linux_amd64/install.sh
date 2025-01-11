fc84a3d49f7b5291a2978889000947c85fb69056
David Haifley <dhaifley@gmail.com>
2025-01-11 00:15:25 -0500

    fix(OPL-11304): Fix a consistent version for the linux fluent-bit package

--- packages/.fluent-bit/linux/fluent-bit_3.2.3_linux_amd64/install.sh
+++ packages/.fluent-bit/linux/fluent-bit_3.2.3_linux_amd64/install.sh
@@ -6,7 +6,7 @@ wget -O fbinstall.sh https://raw.githubusercontent.com/fluent/fluent-bit/master/
 
 if $DIR/checkmd5 --hash=d9a311aba2b23f9e3d87a3eec426d1ae8984f88f5126e256e25bae4f9fb10fc07db563e3d56e9ef5098a8976fca27939377bd48871d28040bbf2d9841fbdfe40 --file=fbinstall.sh; then
     chmod +x ./fbinstall.sh;
-    ./fbinstall.sh;
+    FLUENT_BIT_RELEASE_VERSION=3.2.3 ./fbinstall.sh;
     rm fbinstall.sh;
     systemctl disable fluent-bit;
 else
